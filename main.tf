aa4b5193b020b625135222d8d012a5436f59e8e8
Toshakins <toshakins@gmail.com>
2018-02-07 13:45:40 +0200

    Corrected connection to the EC2 instance

---
+++ main.tf
@@ -0,0 +1,52 @@
+# Specify the provider and access details
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_key_pair" "auth" {
+  key_name   = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
+
+resource "aws_instance" "web" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    # The default username for our AMI
+    user = "ubuntu"
+
+    # The connection will use the local SSH agent for authentication.
+  }
+
+  instance_type = "t2.micro"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = "${lookup(var.aws_amis, var.aws_region)}"
+
+  # The name of our SSH keypair we created above.
+  key_name = "${aws_key_pair.auth.id}"
+
+  # The name of the instance
+  tags {
+    Name = "selenium"
+  }
+
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = ["${var.security_group}"]
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  subnet_id = "${var.subnet_id}"
+
+  # We run a remote provisioner on the instance after creating it.
+  # In this case, we just install nginx and start it. By default,
+  # this should be on port 80
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update",
+      "wget -c http://selenium-release.storage.googleapis.com/3.8/selenium-server-standalone-3.8.0.jar",
+    ]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,8 +1,38 @@
 variable "public_key_path" {
-  description = "EC2 Public Key Path"
-  default = "~/.ssh/selenium"
+  description = <<DESCRIPTION
+Path to the SSH public key to be used for authentication.
+Ensure this keypair is added to your local SSH agent so provisioners can
+connect.
+
+Example: ~/.ssh/terraform.pub
+DESCRIPTION
 }
 
-variable "name" {
-  default = "selenium"
+variable "key_name" {
+  description = "Desired name of AWS key pair"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "eu-central-1"
+}
+
+# Ubuntu Precise 12.04 LTS (x64)
+variable "aws_amis" {
+  type = "map"
+  default = {
+    "eu-west-1"     = "ami-674cbc1e"
+    "us-east-1"     = "ami-1d4e7a66"
+    "us-west-1"     = "ami-969ab1f6"
+    "us-west-2"     = "ami-8803e0f0"
+    "eu-central-1"  = "ami-5055cd3f"
+  }
+}
+
+variable "security_group" {
+  description = "Security Group to assign to EC2 instance"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID to assign to the EC2 instance"
 }
